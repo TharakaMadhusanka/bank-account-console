00e03aa03af6ec45ead739b89abfa3d5a444c4c5
Tharaka_Madhusanka <tharakalearn@gmail.com>
2024-10-26 09:29:26 +0800

    Added Initial project set up | DI Console

---
+++ BankConsole/ConsoleInterfaceService.cs
@@ -0,0 +1,12 @@
+﻿namespace BankConsole
+{
+    internal class ConsoleInterfaceService: IConsoleInterfaceService
+    {
+        public ConsoleInterfaceService() { }
+        public void Start()
+        {
+            Console.WriteLine("Print Menu");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ BankConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using BankConsole;
+using Microsoft.Extensions.DependencyInjection;
+
+var services = new ServiceCollection();
+services.AddSingleton<IConsoleInterfaceService, ConsoleInterfaceService>();
+// DI Container
+var serviceContainer = services.BuildServiceProvider();
+
+// intialize trigger Console Application
+var console = serviceContainer.GetRequiredService<IConsoleInterfaceService>();
+console.Start();
+
